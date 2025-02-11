65d96b710d2e4d4906ff6e416379d3868816917c
Yuheni Ivaniuk <jaugeni@gmail.com>
2025-02-10 18:53:13 -0700

    project settings initial commit

---
+++ AiChatApp/AiChatApp/AiChatAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  AiChatAppApp.swift
+//  AiChatApp
+//
+//  Created by Yauheni Ivaniuk on 2/10/25.
+//
+
+import SwiftUI
+
+@main
+struct AiChatAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AiChatApp/AiChatApp/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  AiChatApp
+//
+//  Created by Yauheni Ivaniuk on 2/10/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
