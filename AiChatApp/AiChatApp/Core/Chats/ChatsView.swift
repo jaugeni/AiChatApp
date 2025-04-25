51a411024bcfacd900cebe09df7dae7fe222eaa8
Yuheni Ivaniuk <jaugeni@gmail.com>
2025-04-25 08:46:13 -0600

    chats model

--- AiChatApp/AiChatApp/Core/Chats/ChatsView.swift
+++ AiChatApp/AiChatApp/Core/Chats/ChatsView.swift
@@ -6,10 +6,17 @@
 //
 import SwiftUI
 struct ChatsView: View {
+
+    @State private var chats: [ChatModel] = ChatModel.mocks
+
     var body: some View {
         NavigationStack {
-            Text("Chats")
-                .navigationTitle("Charts")
+            List {
+                ForEach(chats) { chat in
+                    Text(chat.id)
+                }
+            }
+            .navigationTitle("Chats")
         }
     }
 }
