0bee409804c29d7af14d37725ae1f39abf301c8f
Yuheni Ivaniuk <jaugeni@gmail.com>
2025-06-25 13:36:55 -0600

    some changes need to check what is done

---
+++ AiChatApp/AiChatApp/Components/Modals/ProfileModalView.swift
@@ -0,0 +1,18 @@
+//
+//  ProfileModalView.swift
+//  AiChatApp
+//
+//  Created by Yauheni Ivaniuk on 4/30/25.
+//
+
+import SwiftUI
+
+struct ProfileModalView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ProfileModalView()
+}
